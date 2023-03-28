264ea2ff45e76d79577495f4d14856c1278b35cb
orsys <dev@orsys.fr>
2023-03-28 11:16:29 +0200

    Sub Routing

--- projects/klee-atlas/src/app/features/feat-auth/feat-auth-routing.module.ts
+++ projects/klee-atlas/src/app/features/feat-auth/feat-auth-routing.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLoginComponent } from './components/auth-login/auth-login.component';
+import { AuthRegisterComponent } from './components/auth-register/auth-register.component';
 import { FeatAuthComponent } from './feat-auth.component';
 
 const routes: Routes = [
   {
-    path:/* auth/ */'login',
-    component:FeatAuthComponent
-  },
-  {
-    path:/* auth/ */'register',
-    component:FeatAuthComponent
-  },
-  {
-    path:'**',
-    redirectTo:'register',
-    pathMatch:'prefix'
-  },
+    path:/* auth */'',
+    component:FeatAuthComponent,
+    children:[
+      {
+        path:/* auth/ */'login',
+        component:AuthLoginComponent
+      },
+      {
+        path:/* auth/ */'register',
+        component:AuthRegisterComponent
+      },
+      {
+        path:'**',
+        redirectTo:'register',
+        pathMatch:'prefix'
+      }
+    ]
+  }
 ];
 
 @NgModule({
