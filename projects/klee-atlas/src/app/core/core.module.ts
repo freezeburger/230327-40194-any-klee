69049cf50991bbdb4af41db2cc4dfd8d2adbe117
orsys <dev@orsys.fr>
2023-03-28 13:49:44 +0200

    Angular Application LlifeCycle

--- projects/klee-atlas/src/app/core/core.module.ts
+++ projects/klee-atlas/src/app/core/core.module.ts
@@ -1,19 +1,23 @@
-import { NgModule , APP_INITIALIZER} from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BusEventService } from './services/utils/bus-event.service';
 import { GLOBAL_EVENT_BUS } from './tokens/global-event-bus.token';
 
 @NgModule({
-  providers:[
+  providers: [
     BusEventService,
     {
-      provide:GLOBAL_EVENT_BUS,
-      useExisting:BusEventService
+      provide: GLOBAL_EVENT_BUS,
+      useExisting: BusEventService
     },
     {
-      provide:APP_INITIALIZER,
-      useValue:() => alert('Application Just Started'),
-      multi:true
+      provide: APP_INITIALIZER,
+      useValue: () => console.warn('Before components !!'),
+      multi: true
     }
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor() {
+    console.warn('Modules Time !', this)
+  }
+}
--- projects/klee-atlas/src/main.ts
+++ projects/klee-atlas/src/main.ts
@@ -1,7 +1,17 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { PLATFORM_INITIALIZER } from '@angular/core';
+import { platformBrowserDynamic,  } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic(
+  [
+    {
+      provide:PLATFORM_INITIALIZER,
+      useValue:() => console.warn('Before Modules !!'),
+      multi:true
+    }
+  ]
+)
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
