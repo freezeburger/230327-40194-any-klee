dcaa4913afd995f75e5967c489759ceb65d63407
orsys <dev@orsys.fr>
2023-03-29 11:43:50 +0200

    Message Compoennt

--- projects/klee-atlas/src/app/features/feat-message/feat-message-routing.module.ts
+++ projects/klee-atlas/src/app/features/feat-message/feat-message-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeatMessageComponent } from './feat-message.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:FeatMessageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
